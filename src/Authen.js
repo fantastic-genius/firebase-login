2ae6337ec2d109ed5d66215caa53c10e2903d93d
fantastic-genius <fantastigenius@yahoo.com>
2018-08-15 22:42:55 +0100

    first commit

---
+++ src/Authen.js
@@ -0,0 +1,117 @@
+import React, {Component} from 'react';
+var firebase = require('firebase');
+
+var config = {
+    apiKey: "AIzaSyDpsBCFGNqAtUhxPE8U6oJXx29oFtDj7Zw",
+    authDomain: "fir-login-fd77c.firebaseapp.com",
+    databaseURL: "https://fir-login-fd77c.firebaseio.com",
+    projectId: "fir-login-fd77c",
+    storageBucket: "fir-login-fd77c.appspot.com",
+    messagingSenderId: "143136326613"
+  };
+  firebase.initializeApp(config);
+
+
+class Authen extends Component {
+  login(){
+    const email = this.refs.email.value;
+    const password = this.refs.password.value;
+
+    console.log(email, password);
+    const auth = firebase.auth();
+
+    const promise = auth.signInWithEmailAndPassword(email, password);
+
+    promise.then(response => {
+      var lout = document.getElementById('logout');
+      var err = "Welcome " + response.user.email;
+      lout.classList.remove('hide');
+      this.setState({err: err});
+    });
+
+    promise.catch(e => {
+      var err = e.message;
+      console.log(err);
+      this.setState({err: err});
+    });
+  }
+
+  signup(){
+    const email = this.refs.email.value;
+    const password = this.refs.password.value;
+
+    console.log(email, password);
+    const auth = firebase.auth();
+
+    const promise = auth.createUserWithEmailAndPassword(email, password);
+
+    promise
+    .then(response => {
+      var err = "Welcome " + response.user.email;
+      console.log(response.user);
+      firebase.database().ref('users/'+response.user.uid).set({
+        email: response.user.email
+      });
+
+      this.setState({err: err});
+
+    }).catch(e => {
+      var err = e.message;
+      this.setState({err: err});
+    });
+
+  }
+
+  logout(){
+    firebase.auth().signOut();
+
+    var lout = document.getElementById('logout');
+    var err = "Thank you for using this application";
+    lout.classList.add('hide');
+    this.setState({err: err});
+  }
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      err: ''
+    };
+    this.login = this.login.bind(this);
+    this.signup = this.signup.bind(this);
+    this.logout = this.logout.bind(this);
+    this.google = this.google.bind(this);
+  }
+
+  google(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+    var promise = firebase.auth().signInWithPopup(provider);
+
+    promise.then(result => {
+      var user = result.user;
+      firebase.database().ref('users/'+ user.uid).set({
+        email: user.email,
+        name: user.displayName
+      });
+    }).catch(e => {
+      var err = e.message;
+      this.setState({err:err});
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        <input id="email" ref="email" type="email" placeholder="Enter your email" /><br />
+        <input id="pass" ref="password" type="password" placeholder="Enter your password" /><br />
+        <p>{this.state.err}</p>
+        <button onClick={this.login}>Log In</button>
+        <button onClick={this.signup}>Sign Up</button>
+        <button onClick={this.logout} id="logout" className="hide">Log Out</button><br />
+        <button onClick={this.google} id="google" className="google">Sign In with Google</button>
+      </div>
+    );
+  }
+}
+
+export default Authen;
